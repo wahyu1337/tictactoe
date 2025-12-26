277dcd8077a70d49238d504d79ed04a3d88a4096
wahyu1337 <wahyuisnaeni2002@gmail.com>
2025-12-26 14:25:54 +0800

    ADD
    Logical things in console.log(),
    push array & reset function + retrieve index

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 // gameboard module
-
 const gameBoard = (function (){
-    const board = ["", "", "", "", "", "", "", "", ""];
-
-    const getBoard = () => {
-        for (let i = 0; i < 9; i++){
-            if(i % 2 === 0){
-                board.push("O");
-            } else {
-                board.push("X");
-            }
+    const boards = ["", "", "", "", "", "", "", "", ""];
+    // get the board index
+    const retrieveboardIndex = () => boards;
+    // put the marker in board
+    const retrieveBoardMarker = (index, marker) => {
+        boards[index] = marker;
+    };
+    // reset the board
+    const resetBoard = () => {
+        for (let i = 0; i < boards.length; i++){
+            boards[i] = "";
         }
+    };
+    return{retrieveboardIndex, retrieveBoardMarker, resetBoard};
+})();;
 
-    }
-
-    return {getBoard};
-})();
+const play = gameBoard;
+console.log(play.retrieveboardIndex());
