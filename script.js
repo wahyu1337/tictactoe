f98d2ed9e6c900bac009fe3aeac4e2884f0e1366
wahyu1337 <wahyuisnaeni2002@gmail.com>
2025-12-26 15:54:07 +0800

    ADD
    ADDING create player function

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 // gameboard module
 const gameBoard = (function (){
     const boards = ["", "", "", "", "", "", "", "", ""];
+
     // get the board index
-    const retrieveboardIndex = () => boards;
+    const retrieveBoardIndex = () => boards;
+
     // put the marker in board
     const retrieveBoardMarker = (index, marker) => {
         boards[index] = marker;
     };
+
     // reset the board
     const resetBoard = () => {
         for (let i = 0; i < boards.length; i++){
             boards[i] = "";
         }
     };
-    return{retrieveboardIndex, retrieveBoardMarker, resetBoard};
+
+    return {boards, retrieveBoardIndex, retrieveBoardMarker, resetBoard};
 })();;
 
-const play = gameBoard;
-console.log(play.retrieveboardIndex());
+// create player func
+const createPlayer = function(name, marker){
+    return {name, marker};
+}
+
+const player1 = createPlayer("Ways", "X");
+const player2 = createPlayer("Kinan", "O");
+
+// create game controller for tracks the array index
+
+
+console.log(player1);
+console.log(player2);
