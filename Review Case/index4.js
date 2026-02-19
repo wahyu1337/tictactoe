f24d14fbde361c6cab8327c8482630722e304361
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-02-19 16:16:29 +0800

    Review Turn Info player

--- Review Case/index4.js
+++ Review Case/index4.js
@@ -10,8 +10,21 @@ const gameSession = (function(){
     const player1 = createPlayer("WAYS", "X");
     const player2 = createPlayer("Kinan", "O");
 
-    return{player1,player2};
+    let currentPlayer = player1;
+
+    function switchPlayer(){
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+
+    function getCurrentPlayer(){
+        console.log(`${currentPlayer.name}'s Turn!`);
+    }
+
+    return{player1,player2, switchPlayer, getCurrentPlayer};
 })()
 
 gameSession.player1.getInfo();
 gameSession.player2.getInfo();
+gameSession.getCurrentPlayer();
+gameSession.switchPlayer();
+gameSession.getCurrentPlayer();
