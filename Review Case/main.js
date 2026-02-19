027f50d6c94a7c8c4199f80fc5fe1d2bf7c5ce3e
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-02-20 00:41:12 +0800

    Add Gameboard & Create player
    Create Player menggunakan Factory Function

---
+++ Review Case/main.js
@@ -0,0 +1,35 @@
+const gameBoard = (function (){
+    // Empty Board
+    const board =  ["", "", "",
+                    "", "", "",
+                    "", "", "",]  
+    
+    // Print first Board value
+    const printBoard = () => console.log(board);
+
+
+    // Reset the board
+    function resetBoard(){
+        for (let i = 0; i < board.length; i++){
+            board[i] = "";
+        }
+    }
+
+    return {board, printBoard, resetBoard};
+})()
+
+const createPlayer = function(name, marker){
+    // Retrieve player's information
+    function getPlayerInfo(){
+        console.log(`Player: ${name} | Marker: ${marker}`)
+    }
+
+    return {getPlayerInfo}
+}
+
+const player1 = createPlayer("Ways", "X");
+const player2 = createPlayer("Kinan", "O");
+
+console.log('-------------------OUTPUT-------------------')
+player1.getPlayerInfo();
+player2.getPlayerInfo();
